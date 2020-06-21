b5644498dca1987517410b62f63e3c3fd26dca89
Margo <margo.bozhenova@gmail.com>
2020-06-21 08:51:24 +0300

    Add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,21 @@
+import './spinner.css';
+import React from 'react';
+
+const Spinner = () => {
+  return (
+    <div className='loadingio-spinner-double-ring-06ffj4bbadfx'>
+      <div className='ldio-bq9h1bvhxtj'>
+        <div></div>
+        <div></div>
+        <div>
+          <div></div>
+        </div>
+        <div>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
