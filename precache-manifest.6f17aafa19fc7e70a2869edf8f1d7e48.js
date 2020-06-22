a95a2865db49275c2309af8a399ca335aa972e58
Margo <margo.bozhenova@gmail.com>
2020-06-23 00:05:59 +0300

    Updates

---
+++ precache-manifest.6f17aafa19fc7e70a2869edf8f1d7e48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d9cd8af3a2661fb4b049f381bd9726",
+    "url": "/bozhenova/test-assignment/index.html"
+  },
+  {
+    "revision": "6ca5ed9a15702d3ff571",
+    "url": "/bozhenova/test-assignment/static/css/main.65b5f873.chunk.css"
+  },
+  {
+    "revision": "0d8c77e8006a2308401e",
+    "url": "/bozhenova/test-assignment/static/js/2.6e5b98d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bozhenova/test-assignment/static/js/2.6e5b98d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ca5ed9a15702d3ff571",
+    "url": "/bozhenova/test-assignment/static/js/main.b670682f.chunk.js"
+  },
+  {
+    "revision": "2f2d4440c681a0a3e0e6",
+    "url": "/bozhenova/test-assignment/static/js/runtime-main.8f6cd733.js"
+  },
+  {
+    "revision": "be237328d9c252b1b9133a02c3be3e5e",
+    "url": "/bozhenova/test-assignment/static/media/next.be237328.svg"
+  },
+  {
+    "revision": "6c9efe0c8c1fb630cd48d0381ece4297",
+    "url": "/bozhenova/test-assignment/static/media/prev.6c9efe0c.svg"
+  }
+]);
