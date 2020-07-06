d51963e033d4d850b44683d335c74fcd3dd91d75
Margo <margo.bozhenova@gmail.com>
2020-07-07 01:43:57 +0300

    Add debounce to updateData method

---
+++ src/utils/debounce.js
@@ -0,0 +1,15 @@
+const debounce = (func, wait) => {
+  let timeout;
+  return function () {
+    const context = this;
+    const args = arguments;
+    const later = function () {
+      timeout = null;
+      func.apply(context, args);
+    };
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+  };
+};
+
+export default debounce;
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,6 @@
 import compose from './compose';
 import parseDate from './parseDate';
 import parseStargazersCount from './parseStargazersCount';
+import debounce from './debounce';
 
-export { compose, parseDate, parseStargazersCount };
+export { compose, parseDate, parseStargazersCount, debounce };
